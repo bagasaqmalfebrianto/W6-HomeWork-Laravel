f662447b3f0b896ea684eede536149593d9c548c
AndykaSalom <andykasalom@gmail.com>
2023-10-01 12:18:41 +0700

    edit

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,13 +1,128 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>User Profile</title>
+    <!-- Include Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
- <h1>INI SUDAH MASUK KE HALAMAN DASHBOARD</h1>
- <h3>EDIT AJA YA DISINI</h3>
+    <div class="container mt-4">
+        <h1>INI SUDAH MASUK KE HALAMAN DASHBOARD</h1>
+        <h3>EDIT Profile</h3>
+
+        <!-- Profile Display Card -->
+        <div class="card mb-4">
+            <div class="card-body">
+                <h5 class="card-title">User Information</h5>
+                <p class="card-text"><strong>Nama:</strong> {{ $user->name }}</p>
+                <p class="card-text"><strong>Email:</strong> {{ $user->email }}</p>
+                <p class="card-text"><strong>No telpon:</strong> {{ $user->no_telp }}</p>
+            </div>
+        </div>
+
+        <!-- Edit Profile Card -->
+        <div class="card mb-4">
+            <div class="card-header">{{ __('Edit Profile') }}</div>
+            <div class="card-body">
+            <form method="POST" action="{{ route('profile.update') }}">
+                            @csrf
+                            @method('PUT')
+
+                            <div class="form-group row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nama') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name', $user->name) }}" required autocomplete="name" autofocus>
+
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email', $user->email) }}" required autocomplete="email">
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="no_telp" class="col-md-4 col-form-label text-md-right">{{ __('No telpon') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="no_telp" type="text" class="form-control @error('no_telp') is-invalid @enderror" name="no_telp" value="{{ old('no_telp', $user->no_telp) }}" autocomplete="no_telp">
+
+                                    @error('no_telp')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Update Profile') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+            </div>
+        </div>
+
+        <!-- Edit Password Card -->
+        <div class="card">
+            <div class="card-header">{{ __('Change Password') }}</div>
+            <div class="card-body">
+            <form action="{{ route('profile.updatePassword') }}" method="POST">
+            @csrf
+            @method('PUT')
+
+            <div class="form-group">
+                <label for="current_password">Current Password</label>
+                <input type="password" name="current_password" id="current_password" class="form-control" required>
+                @error('current_password')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="new_password">New Password</label>
+                <input type="password" name="new_password" id="new_password" class="form-control" required>
+                @error('new_password')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="new_password_confirmation">Confirm New Password</label>
+                <input type="password" name="new_password_confirmation" id="new_password_confirmation" class="form-control" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Update Password</button>
+        </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Include Bootstrap JS (optional) -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\loginController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,3 +31,10 @@ Route::resource('/login', loginController::class);
 
 //Dashboard
 Route::resource('/dashboard',DashboardController::class)->middleware('auth');
+
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+Route::delete('/profile', [ProfileController::class,'destroy'])->name('profile.destroy');
+Route::get('/profile/edit-password', [ProfileController::class, 'editPassword'])->name('profile.editPassword');
+Route::put('/profile/update-password', [ProfileController::class, 'updatePassword'])->name('profile.updatePassword');
