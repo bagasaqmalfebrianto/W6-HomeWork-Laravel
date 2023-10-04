9d6797c763e1e6a99f3f11e2bbb0e478db32ca7f
bagasaqmalfebrianto <bagasaqmal70@gmail.com>
2023-10-04 19:48:02 +0700

    update home

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+Route::get('/', function () {
+    return view('home', [
+        'title' => 'Home'
+
+    ]);
+});
 Route::get('/home', function () {
     return view('home', [
         'title' => 'Home'
